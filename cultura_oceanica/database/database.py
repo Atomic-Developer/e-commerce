212dbca7233442af8f873a687bddfb612803104a
DANILO SOARES GOMES <danilosoares.pro@gmail.com>
2025-11-28 12:11:08 -0300

    Add files via upload

---
+++ cultura_oceanica/database/database.py
@@ -0,0 +1,3 @@
+from peewee import SqliteDatabase
+
+db = SqliteDatabase('contas_clientes.db')
---
+++ cultura_oceanica/static/haburguer-nav.js
@@ -0,0 +1,54 @@
+class MobileNavbar {
+  constructor(mobileMenu, navList, navLinks) {
+    this.mobileMenu = document.querySelector(mobileMenu);
+    this.navList = document.querySelector(navList);
+    this.navLinks = document.querySelectorAll(navLinks);
+    this.activeClass = "active";
+
+    this.handleClick = this.handleClick.bind(this);
+    this.closeMenu = this.closeMenu.bind(this);
+  }
+
+  animateLinks() {
+    this.navLinks.forEach((link, index) => {
+      link.style.animation
+        ? (link.style.animation = "")
+        : (link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`);
+    });
+  }
+
+  handleClick() {
+    this.navList.classList.toggle(this.activeClass);
+    this.mobileMenu.classList.toggle(this.activeClass);
+    this.animateLinks();
+  }
+
+  // 👉 nova função pra fechar o menu quando clicar num link
+  closeMenu() {
+    this.navList.classList.remove(this.activeClass);
+    this.mobileMenu.classList.remove(this.activeClass);
+    this.navLinks.forEach(link => (link.style.animation = ""));
+  }
+
+  addClickEvent() {
+    this.mobileMenu.addEventListener("click", this.handleClick);
+    // adiciona evento de clique em cada link do menu
+    this.navLinks.forEach(link => {
+      link.addEventListener("click", this.closeMenu);
+    });
+  }
+
+  init() {
+    if (this.mobileMenu) {
+      this.addClickEvent();
+    }
+    return this;
+  }
+}
+
+const mobileNavbar = new MobileNavbar(
+  ".mobile-menu",
+  ".nav-list",
+  ".nav-list li"
+);
+mobileNavbar.init();
